bcd62e54117f40188fbc5ac8265de0fd6f0a6262
safwanbin2 <safwanridwan321@gmail.com>
2024-01-19 23:10:14 +0600

    login-hook-setup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,48 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: { id: "2024010014", password: "safwan" },
+  });
+  const [login, { data, error }] = useLoginMutation();
+
+  const handleLogin = async (data) => {
+    await login(data);
+  };
+
+  console.log(data);
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
       <h2>Login</h2>
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <div>
+          <label htmlFor="id">ID: </label>
+          <input {...register("id")} type="text" id="id" />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input {...register("password")} type="text" id="password" />
+        </div>
+        <div>
+          <Button htmlType="submit" type="primary">
+            Login
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
