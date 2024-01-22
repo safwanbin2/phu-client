d79ed78f19cbb8ffeefa77ff6297d2a05ddbf561
safwanbin2 <safwanridwan321@gmail.com>
2024-01-22 23:58:34 +0600

    redux-custom base query

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -13,6 +13,11 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery,
