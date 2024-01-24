dac95ed66eebf3a759cbc92da67cdbcd6ec61065
safwanbin2 <safwanridwan321@gmail.com>
2024-01-24 18:00:39 +0600

    controller/provider(react-hook-form)

--- src/components/form/PHinput.tsx
+++ src/components/form/PHinput.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { useFormContext } from "react-hook-form";
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
 
-const PHinput = ({ type, name }) => {
-  const { register } = useFormContext();
-  return <input type={type} id="name" {...register(name)} />;
+const PHinput = ({ type, name }: Record<string, string>) => {
+  return (
+    <Controller
+      name={name}
+      render={({ field }) => <Input type={type} id={name} {...field} />}
+    />
+  );
+  // return <input type={type} id="name" {...register(name)} />;
 };
 
 export default PHinput;
